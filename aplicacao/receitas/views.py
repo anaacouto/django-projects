daf00516863e95fb8ce27d5b343558f05a5c04f5
Ana Couto <ana.couto@armateus.com.br>
2021-05-10 17:53:54 -0300

    HelloWorld e Aplicação de receitas da Alura.'

---
+++ aplicacao/receitas/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de chocolate'
+    }
+    dados = {'nomes_das_receitas': receitas}
+    return render(request, 'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
